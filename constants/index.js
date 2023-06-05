b448809c043290a52d6649f063ca5330343fcd4c
dchateaur <d.castillo0130@gmail.com>
2023-06-05 13:46:08 -0600

    Drawer sizes defined

--- constants/index.js
+++ constants/index.js
@@ -5,6 +5,8 @@ const language = {
   },
 };
 
+const SMALL_DRAWER_WIDTH = 120;
+const MEDIUM_DRAWER_WIDTH = 200;
 const DRAWER_WIDTH = 250;
 
-module.exports = { DRAWER_WIDTH, language };
+module.exports = { DRAWER_WIDTH, MEDIUM_DRAWER_WIDTH, SMALL_DRAWER_WIDTH, language };
