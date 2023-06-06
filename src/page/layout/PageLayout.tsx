9de3f306a228509886ca7b8297e3cab6dcc8258e
dchateaur <d.castillo0130@gmail.com>
2023-06-06 14:13:10 -0600

    Drawer width properties sent to NavBar props

--- src/page/layout/PageLayout.tsx
+++ src/page/layout/PageLayout.tsx
@@ -3,19 +3,26 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 // import NavBar from "../../UI/components/NavBar";
 import { NavBar, Footer } from "@ui/components";
-import { DRAWER_WIDTH } from "../../../constants/index";
-
+// import { DRAWER_WIDTH } from "../../../constants/index";
+import { DRAWER_WIDTH, MEDIUM_DRAWER_WIDTH, SMALL_DRAWER_WIDTH } from "../../../constants";
 // const DRAWER_WIDTH: number = 250;
 
 type Props = {
   children: React.ReactNode;
 };
 
+const DRAWER_WIDTH_PROPS = {
+  xs: SMALL_DRAWER_WIDTH,
+  sm: MEDIUM_DRAWER_WIDTH,
+  md: DRAWER_WIDTH,
+  lg: DRAWER_WIDTH,
+};
+
 const PageLayout = ({ children }: Props) => {
   return (
     <>
       <Box sx={{ display: "flex" }}>
-        <NavBar needsSidebar={false} />
+        <NavBar needsSidebar={false} drawerWidth={DRAWER_WIDTH_PROPS} />
         <Box
           component="main"
           sx={{ flexGrow: 1, p: 0, backgroundColor: "primary.main" }}
