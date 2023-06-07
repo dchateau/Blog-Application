1c7bc55898dcb98efae32b56ccd88a0df83186be
dchateaur <d.castillo0130@gmail.com>
2023-06-07 14:05:56 -0600

    Styling and screen breakpoints changed

--- theme/mainTheme.ts
+++ theme/mainTheme.ts
@@ -1,15 +1,24 @@
 import { createTheme } from "@mui/material";
 
 export const mainTheme = createTheme({
-    palette: {
-        primary: {
-            main: "#F2F2F2",
-            dark: "#000000",
-        },
-        secondary: {
-            main: "#A6A6A6",
-            dark: "#8C0303"
-            // dark: "#698EBF",
-        },  
-    }
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 950,
+      lg: 1200,
+      xl: 1536
+    },
+  },
+  palette: {
+    primary: {
+      main: "#F2F2F2",
+      dark: "#000000",
+    },
+    secondary: {
+      main: "#A6A6A6",
+      dark: "#8C0303",
+      // dark: "#698EBF",
+    },
+  },
 });
