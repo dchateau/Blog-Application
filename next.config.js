172057dc36b3d1f87fe2243f98240fca7197341a
dchateaur <d.castillo0130@gmail.com>
2023-06-10 14:38:47 -0600

    CORS probably solved?

--- next.config.js
+++ next.config.js
@@ -11,6 +11,24 @@ const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://links.services.disqus.com/api/ping",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.youtube.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
